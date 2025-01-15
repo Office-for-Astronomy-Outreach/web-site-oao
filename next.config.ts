c75ddc5b5e00e36f7657dd91cf6ebcdea884c283
Montserrat Delgado Alvarez <lightwood@MacBook-Air-de-Montserrat.local>
2025-01-14 19:16:12 -0600

    fix: next config images domain delete

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     defaultLocale: 'en', 
     locales: ['en', 'es'],
   },
-  images: {
-    domains: ['static.nationalgeographic.es'], // Agrega el dominio de tus imágenes
-  },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '', // Lee el prefijo de una variable de entorno
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   distDir: 'out'
