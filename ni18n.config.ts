b62ec4c6e7fd80d73b087f744844cd3e0effe0ff
Montserrat Delgado Alvarez <lightwood@MacBook-Air-de-Montserrat.local>
2025-01-15 00:38:13 -0600

    fix: translations

--- ni18n.config.ts
+++ ni18n.config.ts
@@ -3,11 +3,18 @@ import type { Ni18nOptions } from 'ni18n';
 export const ni18nConfig: Ni18nOptions = {
   supportedLngs: ['en'],
   fallbackLng: 'en',
-  ns: ['layout'],
+  ns: ['layout', 'home', 'about'],
+  defaultNS: 'layout',
   interpolation: {
     escapeValue: false, // Esto permite usar HTML
   },
   react: {
     transSupportBasicHtmlNodes: true, // Habilita soporte para etiquetas básicas
+  },
+  backend: {
+    loadPath:
+      process.env.NODE_ENV === 'production'
+        ? '/web-site-oao/locales/{{lng}}/{{ns}}.json' // Ruta para producción
+        : '/locales/{{lng}}/{{ns}}.json', // Ruta para desarrollo local
   },
 };
